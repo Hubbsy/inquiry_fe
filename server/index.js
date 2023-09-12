62d14e57a0cdace941718c4dc4390bb4e309559a
Justin Hubbell <jhubbell@rsitex.com>
2023-09-12 15:40:29 -0500

    new build for actions test

--- server/index.js
+++ server/index.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-require('dotenv').config()
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-
 app.listen(PORT, () => {
-    console.log(`SERVER LISTENING ON PORT ${PORT}`)
-})
+  console.log(`SERVER LISTENING ON PORT ${PORT}`);
+});
