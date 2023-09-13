b029d8ddf079efec05145c7fbb1f6569ae930fad
Justin Hubbell <jhubbell@rsitex.com>
2023-09-13 12:27:56 -0500

    updated server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname)));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
