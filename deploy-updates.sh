04a1cb53246fb2bcfb282e4d4ecf60c65514ecf8
Justin Hubbell <jhubbell@rsitex.com>
2023-09-15 13:46:38 -0500

    updated bash script to include docker deploy

--- deploy-updates.sh
+++ deploy-updates.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Define the branch to pull
-BRANCH="main"
+BRANCH="jwh-github-actions"
 
 # Change to the root directory of the current git repository
 cd "$(git rev-parse --show-toplevel)"
 
 # Pull the latest changes from the specified branch
 git pull origin "$BRANCH"
+
+sudo docker stack deploy --compose-file docker-compose-swarm.yml inquiry_fe
